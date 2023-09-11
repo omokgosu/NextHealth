3c88b195cc4595b825752896f47f67bbf6a99adc
양진성 <anfakt0606@naver.com>
2023-09-11 16:35:57 +0900

    scss setting

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 
+import styles from '@/styles/header.module.scss'
+
 export default function Header() {
  
   return (
-    <header>
+    <header className={styles.header}>
       <h1>
-        <Link href="/">
-            <a><span className="hidden">Food GPT</span></a>
+        <Link href="/" legacyBehavior>
+            <a className={styles.headerLogo}><span className="hidden">Food GPT</span></a>
         </Link>
       </h1>
     </header>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import Header from '@/components/header'
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+
+import Header from '@/components/header'
+
+import '@/styles/globals.scss'
+import '@/styles/constants/color.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
